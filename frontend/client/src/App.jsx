597f2800a2e07a59279a81c00c6573f9a65ace64
AvniDhokia <avDhokia@protonmail.com>
2026-05-03 17:44:09 +0100

    setup front end framework

---
+++ frontend/client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>hiya :3</p>
+    </>
+  )
+}
+
+export default App
