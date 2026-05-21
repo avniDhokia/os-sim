46a18ffc6164690206bcfeedb5031d959c12df7e
AvniDhokia <av.dhokia@protonmail.com>
2026-05-21 15:52:17 +0100

    processes now have different colours

--- frontend/client/src/components/processComp.jsx
+++ frontend/client/src/components/processComp.jsx
@@ -1,8 +1,12 @@
 
+let colourMap = ['red_process', 'orange_process', 'yellow_process', 'green_process', 'blue_process', 'purple_process', 'pink_process']
+
+
 export default function ProcessComp({pid, name, state, priority, timeRan, timeToRun}){
+    let colour = colourMap[ Number(pid) % colourMap.length ]
 
     return(
-        <div id="process" class="component">
+        <div id="process" class={"component " + colour}>
 
             <h1>{name} ({pid})</h1>
             <h2>State: {state}, Priority: {priority}</h2>
